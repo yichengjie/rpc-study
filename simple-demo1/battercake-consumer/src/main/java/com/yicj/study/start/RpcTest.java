2ae30fe1c3916d4d359befe86f4cb6a9de95e70a
yichengjie <626659321@qq.com>
2019-09-14 11:00:27 +0800

    准备引入netty

--- simple-demo1/battercake-consumer/src/main/java/com/yicj/study/start/RpcTest.java
+++ simple-demo1/battercake-consumer/src/main/java/com/yicj/study/start/RpcTest.java
@@ -6,9 +6,11 @@ import com.yicj.study.service.BatterCakeService;
 //记得运行在battercake-provider端的RpcBootstrap类发布BatterCakeService服务）
 public class RpcTest {
 	public static void main(String[] args) {
+		
 		BatterCakeService batterCakeService = 
 				RpcConsumer.getService(BatterCakeService.class, "127.0.0.1", 20006);
-        String result = batterCakeService.sellBatterCake("双蛋");
+		
+		String result = batterCakeService.sellBatterCake("双蛋");
         System.out.println(result);
 	}
 }
