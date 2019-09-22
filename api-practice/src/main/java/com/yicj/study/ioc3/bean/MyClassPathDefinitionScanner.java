f0ed36279c33b030c9615798cad1601e90370d64
yichengjie <626659321@qq.com>
2019-09-22 22:20:05 +0800

    s

--- api-practice/src/main/java/com/yicj/study/ioc3/bean/MyClassPathDefinitionScanner.java
+++ api-practice/src/main/java/com/yicj/study/ioc3/bean/MyClassPathDefinitionScanner.java
@@ -1,10 +1,12 @@
 package com.yicj.study.ioc3.bean;
 
+import org.springframework.beans.factory.config.BeanDefinitionHolder;
 import org.springframework.beans.factory.support.BeanDefinitionRegistry;
 import org.springframework.context.annotation.ClassPathBeanDefinitionScanner;
 import org.springframework.core.type.filter.AnnotationTypeFilter;
 
 import java.lang.annotation.Annotation;
+import java.util.Set;
 
 //编写扫描器
 //通过自定义的扫描器,扫描指定包下所有被@MyBean 注释的类。
@@ -14,6 +16,19 @@ public class MyClassPathDefinitionScanner extends ClassPathBeanDefinitionScanner
         super(registry);
         this.type = type ;
     }
+
+    @Override
+    public Set<BeanDefinitionHolder> doScan(String... basePackages) {
+        Set<BeanDefinitionHolder> definitionHolders = super.doScan(basePackages);
+        System.out.println("\n----------------------------");
+        System.out.println("definitionHolders num : " + definitionHolders.size());
+        for (BeanDefinitionHolder holder : definitionHolders){
+            System.out.println(holder.getBeanDefinition());
+        }
+        System.out.println("----------------------------");
+        return definitionHolders ;
+    }
+
     /**
      * 注册 过滤器
      */
