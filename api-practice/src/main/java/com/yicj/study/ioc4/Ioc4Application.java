39678188c93541889c18c2c9bc83f0db2dc06bda
yichengjie <626659321@qq.com>
2019-09-22 22:44:50 +0800

    准备改造ioc3

---
+++ api-practice/src/main/java/com/yicj/study/ioc4/Ioc4Application.java
@@ -0,0 +1,11 @@
+package com.yicj.study.ioc4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ioc4Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Ioc4Application.class,args) ;
+    }
+}
---
+++ api-practice/src/main/java/com/yicj/study/ioc4/service/IUserService.java
@@ -0,0 +1,5 @@
+package com.yicj.study.ioc4.service;
+
+public interface IUserService {
+    void hello(String name) ;
+}
