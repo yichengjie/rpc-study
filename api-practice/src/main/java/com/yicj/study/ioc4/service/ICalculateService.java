4b05e1b7ce7c20c9d4c7108f442d01f5cee3cb43
yichengjie <626659321@qq.com>
2019-09-23 11:05:08 +0800

    补充练习

---
+++ api-practice/src/main/java/com/yicj/study/ioc4/service/ICalculateService.java
@@ -0,0 +1,5 @@
+package com.yicj.study.ioc4.service;
+
+public interface ICalculateService {
+    String getResult(String name);
+}
--- api-practice/src/test/java/com/yicj/study/ioc4/MyClassPathDefinitionScannerTest.java
+++ api-practice/src/test/java/com/yicj/study/ioc4/MyClassPathDefinitionScannerTest.java
@@ -1,15 +1,26 @@
 package com.yicj.study.ioc4;
 
+import com.yicj.study.ioc4.service.ICalculateService;
+import com.yicj.study.ioc4.service.IUserService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = Ioc4Application.class)
 public class MyClassPathDefinitionScannerTest {
+    @Autowired
+    private IUserService userService ;
+    @Autowired
+    private ICalculateService calculateService ;
+
     @Test
     public void testSimpleScanner()  {
-        System.out.println("hello world");
+        String helloStr = userService.hello("yicj");
+        String calculateResult = calculateService.getResult("测试");
+        System.out.println("helloStr : " + helloStr);
+        System.out.println("calculateResult : " + calculateResult);
     }
 
 }
