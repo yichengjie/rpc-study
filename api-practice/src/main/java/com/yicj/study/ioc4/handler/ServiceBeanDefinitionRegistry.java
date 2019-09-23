e84fd0039042f1cf2622a03f4afd93db2c1f12a6
yichengjie <626659321@qq.com>
2019-09-23 12:18:29 +0800

    s

--- api-practice/src/main/java/com/yicj/study/ioc4/handler/ServiceBeanDefinitionRegistry.java
+++ api-practice/src/main/java/com/yicj/study/ioc4/handler/ServiceBeanDefinitionRegistry.java
@@ -39,7 +39,6 @@ public class ServiceBeanDefinitionRegistry implements BeanDefinitionRegistryPost
         for (Class beanClazz : beanClazzs) {
             BeanDefinitionBuilder builder = BeanDefinitionBuilder.genericBeanDefinition(beanClazz);
             GenericBeanDefinition definition = (GenericBeanDefinition) builder.getRawBeanDefinition();
-
             //在这里，我们可以给该对象的属性注入对应的实例。
             //比如mybatis，就在这里注入了dataSource和sqlSessionFactory，
             // 注意，如果采用definition.getPropertyValues()方式的话，
@@ -48,12 +47,10 @@ public class ServiceBeanDefinitionRegistry implements BeanDefinitionRegistryPost
             // 如果采用definition.getConstructorArgumentValues()，
             // 则FactoryBean中需要提供包含该属性的构造方法，否则会注入失败
             definition.getConstructorArgumentValues().addGenericArgumentValue(beanClazz);
-
             //注意，这里的BeanClass是生成Bean实例的工厂，不是Bean本身。
             // FactoryBean是一种特殊的Bean，其返回的对象不是指定类的一个实例，
             // 其返回的是该工厂Bean的getObject方法所返回的对象。
             definition.setBeanClass(ServiceFactory.class);
-
             //这里采用的是byType方式注入，类似的还有byName等
             definition.setAutowireMode(GenericBeanDefinition.AUTOWIRE_BY_TYPE);
             registry.registerBeanDefinition(beanClazz.getSimpleName(), definition);
